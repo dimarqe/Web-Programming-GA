868eda17bff4146ce4c0287377f8faded8c8c73a
Dimitri <dimitrianthonyrussell@yahoo.com>
2020-11-14 16:45:41 -0800

    Added function to disable body types based on selected gender

--- javascript/indexScript.js
+++ javascript/indexScript.js
@@ -1,3 +1,27 @@
-function addNew(){
+function addNew() {
+
+}
+
+function changeBodyTypes() {
+    var gender = document.querySelector('input[name="gender"]:checked').value;
+    var bodyTypeOption = document.getElementById("bodyTypes").getElementsByTagName("option");
+    var allowedTypes = [];
+
+    if (gender == "male") {
+        allowedTypes = ["oval", "triangle", "rectangle", "rhomboid", "inverted triangle"];
+    }
+    else {
+        allowedTypes = ["apple", "pear", "pencil", "hourglass", "round"]
+    }
     
+    for (var i = 0; i < bodyTypeOption.length; i++) {
+        if(allowedTypes.includes(bodyTypeOption[i].value)){
+            bodyTypeOption[i].disabled = false;
+        }
+        else{
+            bodyTypeOption[i].disabled = true;
+        }
+    }
+
+    document.getElementById("bodyTypes").value = null;
 }
